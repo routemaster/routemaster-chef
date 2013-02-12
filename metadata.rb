c6aa14076e26b8573faf356c458805e8fa6560ea
Benjamin Woodruff <benjaminwoodruff@ufl.edu>
2013-02-11 22:24:40 -0500

    Adds dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,13 @@ maintainer       "The RouteMaster Project"
 maintainer_email "benjaminwoodruff@ufl.edu"
 license          "GNU AGPL v3.0+"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1-dev"
+version          "0.0.1"
+supports         "ubuntu"
+supports         "debian"
+
+# Dependencies
+depends "sudo",    ">= 2.0.0"  # https://github.com/opscode-cookbooks/sudo
+depends "zsh",     ">= 1.0.0"  # https://github.com/opscode-cookbooks/zsh
+depends "maven",   ">= 0.14.0" # https://github.com/opscode-cookbooks/maven
+depends "git",     ">= 2.2.0"  # https://github.com/opscode-cookbooks/git
+depends "mongodb", ">= 0.12"   # https://github.com/edelight/chef-mongodb
